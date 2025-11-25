bb436940e41e0316efd932d2166da55c5080b60c
aboire <thomas.craipeau@gmail.com>
2025-11-25 14:24:50 +0400

    update

--- src/modules/profil/components/news/index.ts
+++
@@ -1,6 +0,0 @@
-export { NewsContent } from "./NewsContent";
-export { NewsImageGrid } from "./NewsImageGrid";
-export { NewsReactionPicker } from "./NewsReactionPicker";
-export { NewsReactionsModal } from "./NewsReactionsModal";
-export { NewsVoteDisplay } from "./NewsVoteDisplay";
-export { NewsComments } from "./NewsComments";
